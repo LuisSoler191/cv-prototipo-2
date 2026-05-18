083056c1f6b7d4874f863667123a4ea1e7c0629f
LuisPracticas02 <practicas2@imediacomunicacion.com>
2026-05-18 13:11:09 +0200

    fix: irrelevante

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        cors: true,  // Permite solicitudes cruzadas
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
